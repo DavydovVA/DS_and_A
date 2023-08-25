77e3bdafcfc0ad2fdd6a1b19910b3d1ecc31f1a1
DavydovVA <volerej@gmail.com>
2023-08-25 21:03:28 +0300

    implement isSubsequence

--- String/tasks.h
+++ String/tasks.h
@@ -4,6 +4,7 @@
 #define LEETCODE
 
 #include <string>
+#include <vector>
 #include <algorithm>
 
 
@@ -86,6 +87,7 @@ bool isPalindrome(std::string str) {
 #endif
 }
 
+
 std::string DecimalToBinary(int decimal) {
     // O(n log(n)), n = decimal
     std::string binaryStr {"0"};
@@ -120,6 +122,84 @@ int StringToDecimal(const std::string str) {
     }
 
     return decimal;
+}
+
+
+std::vector<std::string> GenerateSubsequences(std::string str) {
+    // O(n 2^n)
+    // To find out all subsequences of a string, we need to iterate through all characters of the
+    // string. We also create a bit counter variable to mark which element position should be
+    // considered to take as a subsequence, also known as a power set. The power set of S is the
+    // set of all subsets of S. Suppose we have three characters in a string, which are xyz. The
+    // power set of the string will be 2n elements, which is as follows:
+    // BIT -> SUBSET
+    // ===================
+    // 000 -> Empty subset
+    // 001 -> "x"
+    // 010 -> "y"
+    // 011 -> "xy"
+    // 100 -> "z"
+    // 101 -> "xz"
+    // 110 -> "yz"
+    // 111 -> "xyz"
+    std::vector<std::string> vec;
+
+    const int strLen = str.size();
+    const int bitCounter = std::pow(2, strLen);
+    for (int i = 1; i < bitCounter; i++) {
+        std::string subseq = "";
+
+        for(int j = 0; j < strLen; j++) {
+            if (i & (1 << j)) {
+                subseq += str[j];
+            }
+        }
+
+        vec.push_back(subseq);
+    }
+
+    return vec;
+}
+
+
+// A subsequence is a sequence that can be derived from another sequence
+// by deleting some elements without changing the order of the remaining elements
+// We suppose str1 is subsequence of str2, x - str1's index, y - str2's
+bool RisSubsequence(
+    const std::string &str1,
+    const std::string &str2,
+    int x,
+    int y)
+{
+    // O(n)
+    if (x == 0) {
+        return true;
+    }
+    if (y == 0) {
+        return false;
+    }
+
+    if (str1[x - 1] == str2[y - 1]) {
+        return RisSubsequence(str1, str2, x - 1, y - 1);
+    }
+
+    return RisSubsequence(str1, str2, x, y - 1);
+}
+
+// Returns true if str1 is subsequence of str2
+bool IisSubsequence(const std::string &str1, const std::string &str2) {
+    // O(n)
+    int n = str1.size(), m = str2.size();
+
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (str1[i] == str2[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return i == n;
 }
 
 
